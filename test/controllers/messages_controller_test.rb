a3097d6460da7f7c28aa79e424be180964e4a6e9
murdoch <stephen@murdo.ch>
2016-11-15 16:02:50 +0000

    Flesh out :create action and view

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -14,4 +14,30 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
       assert_select 'input[type=submit]'
     end
   end
+
+  test "POST create" do
+    post create_message_url, params: {
+      message: {
+        name: 'cornholio',
+        email: 'cornholio@example.org',
+        body: 'hai'
+      }
+    }
+
+    assert_redirected_to new_message_url
+
+    follow_redirect!
+
+    assert_match /Message received, thanks!/, response.body
+  end
+
+  test "invalid POST create" do
+    post create_message_url, params: {
+      message: { name: '', email: '', body: '' }
+    }
+
+    assert_match /Name .* blank/, response.body
+    assert_match /Email .* blank/, response.body
+    assert_match /Body .* blank/, response.body
+  end
 end
