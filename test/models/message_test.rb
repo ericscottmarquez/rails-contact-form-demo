641057676ea43f9bc2f5b63f13e384b6f3c716c1
murdoch <stephen@murdo.ch>
2016-11-15 15:17:18 +0000

    Validate presence of name, email and body

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -20,4 +20,14 @@ class MessageTest < ActiveSupport::TestCase
     msg = Message.new attrs
     assert msg.valid?, 'should be valid'
   end
+
+  test 'name, email and body are required by law' do
+    msg = Message.new
+
+    refute msg.valid?, 'Blank Mesage should be invalid'
+
+    assert_match /blank/, msg.errors[:name].to_s
+    assert_match /blank/, msg.errors[:email].to_s
+    assert_match /blank/, msg.errors[:body].to_s
+  end
 end
